c9a5ab3c55d1e5f68bb453a7fbd116d789bd5c7f
Anonymous <44589447+anonymous-tld@users.noreply.github.com>
2019-01-12 05:35:56 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<meta charset="utf-8">
+<? $_GET['v']; ?>
+<title>anonymous.support - LIVESTREAM</title>
+<link href="live.css" rel="stylesheet">
+</head>
+<body bgcolor="FFFFFF" cellspacing="0" cellpadding="0" leftmargin="0" topmargin="0">
+<meta name="author" content="Video">
+<meta name="DC.Publisher" content="Video">
+<meta name="DC.Subject" content="http://anonymous.support/live">
+<meta name="DC.Identifier" content="http://anonymous.support/live">
+<meta name="image" content="http://img.youtube.com/vi/<?php echo $_GET['v'];?>/hqdefault.jpg">
+<meta name="twitter:image" content="http://img.youtube.com/vi/<?php echo $_GET['v'];?>/hqdefault.jpg"/>
+<meta property="og:image" content="http://img.youtube.com/vi/<?php echo $_GET['v'];?>/hqdefault.jpg" />
+<meta property="og:title" content="VIDEO"/>
+<meta property="og:type" content="movie"/>
+<meta property="og:url" content="http://anonymous.support/live"/>
+<meta property="og:locale" content="de_DE"/>
+</body>
+<table width="100%" height="100%"  align=left valign=top border="0" cellspacing="0" cellpadding="0" leftmargin="0" topmargin="0"><td width=100% height=100% align=left valign=top border="0" cellspacing="0" cellpadding="0" leftmargin="0" topmargin="0"><iframe title="Video Player" src="http://www.youtube.com/embed/zBh5UnFjpZI?html5=1&rel=0&amp;controls=1&autoplay=1&modestbranding=0&loop=1&amp;showinfo=0&playlist=<?php echo $_GET['v'];?>" style="border:none;overflow:hidden" width="100%" height="100%" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true" cellspacing="0" cellpadding="0" leftmargin="0" topmargin="0"></iframe>
+</tr></table>
+</html>
